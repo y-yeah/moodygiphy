d717f7261346dfa24a340014f7378035c6a85365
Yuma Sumi <rbly.776324.yuma66krayt@gmail.com>
2019-02-19 17:10:59 +0900

    added vuetify

---
+++ src/components/Upload.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="upload">
+    <input type="file">
+    <button @click="upload">Upload</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "upload"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
